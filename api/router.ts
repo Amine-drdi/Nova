9c9f55ce1830d900d88245f910fa5bad5741cf86
Amine Dridi <dridiimedamine@gmail.com>
2026-07-01 15:35:44 +0200

    add

--- api/router.ts
+++ api/router.ts
@@ -1,7 +1,7 @@
 import { authRouter } from './auth-router.js';
 import { createRouter, publicQuery } from './middleware.js';
 import { clientRouter } from './client-router.js';
-import { seoTaskRouter } from './seo-task-router.js';
+import seoTaskRouter from './seo-task-router.js';
 import { keywordRouter } from './keyword-router.js';
 import { pageSeoRouter } from './page-seo-router.js';
 import { backlinkRouter } from './backlink-router.js';
